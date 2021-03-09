466a81ca05da5fdabce21191fac6cffe24761be2
Thorin Oakenshield <thror1200@gmail.com>
2021-03-09 18:42:33 +0700

    fixed AuthenUser

--- src/Authen/AuthenUser.js
+++ src/Authen/AuthenUser.js
@@ -1,9 +1,11 @@
 import AuthenToken from "./AuthenToken";
+import AcceptLogin from "../helpers/AcceptLogin";
 
 async function authenUser(account, password) {
   const response = await AuthenToken();
   if (response) {
     if (response.account === account && response.password === password) {
+      AcceptLogin(account, password);
       return true;
     }
   }
