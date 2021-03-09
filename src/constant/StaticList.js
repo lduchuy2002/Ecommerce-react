e10a6f951afd736482f09728a61575d57529d819
Thorin Oakenshield <thror1200@gmail.com>
2021-03-09 20:56:07 +0700

    add static list for rendering

---
+++ src/constant/StaticList.js
@@ -0,0 +1,21 @@
+//Head banner slider images list
+export const BANNER_IMAGES = [
+  "/assets/images/HeadBanner-1.jpg",
+  "/assets/images/HeadBanner-2.jpg",
+  "/assets/images/HeadBanner-3.jpg",
+  "/assets/images/HeadBanner-4.jpg",
+  "/assets/images/HeadBanner-5.jpg",
+];
+
+export const FILTER_RAM_LIST = [
+  { value: "4G", search: "4 GB" },
+  { value: "8G", search: "8 GB" },
+  { value: "16G", search: "16GB" },
+];
+
+export const FILTER_PRICE_LIST = [
+  { value: 10, note: "Dưới 10 triệu" },
+  { value: 15, note: "15 Đến 20 triệu" },
+  { value: 20, note: "20 đến 25 triệu" },
+  { value: 30, note: "Trên 25 triệu" },
+];
