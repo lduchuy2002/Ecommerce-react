7a72c08afec8c324a44099dbbb484c22518fafa0
Thorin Oakenshield <thror1200@gmail.com>
2021-03-09 20:56:49 +0700

    refactor authen user

--- src/Authen/AuthenUser.js
+++ src/Authen/AuthenUser.js
@@ -1,15 +1,17 @@
-import AuthenToken from "./AuthenToken";
 import AcceptLogin from "../helpers/AcceptLogin";
-
-async function authenUser(account, password) {
-  const response = await AuthenToken();
-  if (response) {
-    if (response.account === account && response.password === password) {
-      AcceptLogin(account, password);
+import UserApi from "../api/UserApi";
+async function AuthenUser(account, password) {
+  const user = await UserApi.findToken(account);
+  if (user) {
+    if (user.account === account && user.password === password) {
+      localStorage.setItem("accessToken", user.id);
+      AcceptLogin(user);
       return true;
     }
+  } else {
+    alert("Tài khoản không tồn tại hoặc sai thông tin");
   }
   return false;
 }
 
-export default authenUser;
+export default AuthenUser;
