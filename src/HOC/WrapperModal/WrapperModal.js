d4e52c36c7b5ce42bd747c3bb0e860bfaed1426e
Thorin Oakenshield <thror1200@gmail.com>
2021-03-09 18:42:58 +0700

    fixed wrapper modal

--- src/HOC/WrapperModal/WrapperModal.js
+++ src/HOC/WrapperModal/WrapperModal.js
@@ -1,17 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./WrapperModal.scss";
 import Modal from "../../container/Modal/Modal";
+import { useHistory } from "react-router-dom";
+import getToken from "../../helpers/getToken";
+import UserApi from "../../api/UserApi";
 function WrapperModal() {
-  const [isDisplayModal, setDisplayModal] = useState(true);
+  const [isLogin, setLogin] = useState(false);
+  const history = useHistory();
+  const handleDisplayModal = () => {
+    history.goBack();
+  };
+  useEffect(() => {
+    const autoLogin = async () => {
+      const token = getToken();
+      if (token) {
+        const response = await UserApi.get(token);
+        if (response.isLogin === true) {
+          setLogin(true);
+        }
+      }
+    };
 
+    autoLogin();
+  }, []);
   return (
-    <div
-      className="wrapper-modal"
-      style={{
-        display: !isDisplayModal && "none",
-      }}
-    >
-      <Modal handleDisplayModal={setDisplayModal} />
+    <div className="wrapper-modal">
+      <Modal handleDisplayModal={handleDisplayModal} isLogin={isLogin} setLogin={setLogin} />
     </div>
   );
 }
