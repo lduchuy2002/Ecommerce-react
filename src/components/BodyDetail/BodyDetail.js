c029b5e7b7a87a9f183a3da60e25322bef6f10b8
Thorin Oakenshield <thror1200@gmail.com>
2021-03-10 21:23:50 +0700

    add handle function for addToCart component

--- src/components/BodyDetail/BodyDetail.js
+++ src/components/BodyDetail/BodyDetail.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useParams } from "react-router-dom";
 import "./BodyDetail.css";
 
 import AdjustItem from "../AdjustItem/AdjustItem";
@@ -6,15 +7,36 @@ import AddToCard from "../AddToCart/AddToCart";
 import DetailSlider from "../DetailSlider/DetailSlider";
 import Table from "../TableDetail/TableDetail";
 
+import AuthenToken from "../../Authen/AuthenToken";
+import getToken from "../../helpers/getToken";
+import UserApi from "../../api/UserApi";
+
 function BodyDetail({ data }) {
   const [quantity, setQuantity] = useState(1);
-
+  const { id } = useParams();
   const adjustQuantity = type => {
     if (type === "+") {
       setQuantity(quantity + 1);
     } else {
       if (quantity !== 1) {
         setQuantity(quantity - 1);
+      }
+    }
+  };
+
+  const onAddToCart = async () => {
+    const token = getToken();
+    //Đoạn này code ơi củ chuối
+    if (token) {
+      const user = await AuthenToken(token);
+      if (user) {
+        const alreadyInCart = user.cart.find(product => product.item === id);
+        if (alreadyInCart) {
+          alreadyInCart.quantity += quantity;
+        } else {
+          user.cart.push({ item: id, quantity: quantity });
+        }
+        UserApi.updateUser(token, user);
       }
     }
   };
@@ -28,7 +50,7 @@ function BodyDetail({ data }) {
 
       <div className="wrapper-compo">
         <AdjustItem quantity={quantity} handleFunction={adjustQuantity}></AdjustItem>
-        <AddToCard />
+        <AddToCard onAddToCart={onAddToCart} />
       </div>
     </React.Fragment>
   );
