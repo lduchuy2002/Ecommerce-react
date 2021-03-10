603589026d69a2f2ef954c7b780032775080e80d
Thorin Oakenshield <thror1200@gmail.com>
2021-03-10 21:23:17 +0700

    add handle function for addToCart component

--- src/components/AddToCart/AddToCart.js
+++ src/components/AddToCart/AddToCart.js
@@ -1,9 +1,9 @@
 import React from "react";
 import "./AddToCart.scss";
 
-function AddToCard() {
+function AddToCard({ onAddToCart }) {
   return (
-    <div className="add-to-cart">
+    <div className="add-to-cart" onClick={onAddToCart}>
       <i className="fas fa-shopping-cart"></i>
       <span>Thêm vào giỏ hàng</span>
     </div>
